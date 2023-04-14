5e846002b63b6c0b7c98184d50ec19303e503bdc
anikurhade <anikurhade2002@gmail.com>
2023-04-14 02:08:14 -0700

    Update With Latest Commit

--- Bash Scripts/01_Basics.sh
+++ Bash Scripts/01_Basics.sh
@@ -1,9 +1,7 @@
 # !/bin/sh
-
-
+# Name :- Anirudha
 # * echo is equivalent to printf but it by defaults prints new line
-echo 'Enter a number: '
-read number
-echo 'You have entered ' $number
-echo "You have entered " $number
-printf "You have entered %d \n" $number
+echo 'Enter Your Name: '
+read string
+echo 'You Said : ' $string
+printf "You have entered %s \n" $string
